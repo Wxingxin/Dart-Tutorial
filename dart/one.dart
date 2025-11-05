f978620e4189fbcc93feec44a9f54503924a5b15
WeiJiaXing <weijiaxing765@gmail.com>
2025-11-05 22:37:52 +0800

    11-5

--- dart/one.dart
+++ dart/one.dart
@@ -1,20 +1,26 @@
-class User {
-  String name;
-  int age;
-  User(this.name, this.age);
-}
-
-class Repository<T> {
-  final List<T> _data = [];
-
-  void add(T item) => _data.add(item);
-  void showAll() => _data.forEach((item) => print(item));
-}
-
 void main() {
-  var userRepo = Repository<User>();
-  userRepo.add(User("张三", 20));
-  userRepo.add(User("李四", 22));
+  var p1 = One();
+}
 
-  userRepo.showAll(); // 输出: Instance of 'User'...
+class Two {
+  int x;
+  int y;
+
+  Two.origin(): x = 0, y = 0;
+}
+
+class One {
+  String name = 'gao lixin';
+  One() {
+    print('this is one class');
+  }
+}
+
+class Person {
+  String name = 'no name';
+  int age = 0;
+
+  void sayHello() {
+    print('hello I is $name , my age is $age');
+  }
 }
